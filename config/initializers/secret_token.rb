c7f42925c48154a4dd03575a259813de2bd86699
Nao Minami <minami@minami-no-MacBook-Air.local>
2013-06-17 22:20:36 +0900

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NikoNiko::Application.config.secret_token = '6e4a92a66ecb80f5c88b680f7090c5c8e313a5b7af46d2847ddf84e00b970fdba2fe1e31f7f66f0f2998447f090a580c994afceb86d7c28db80f73c97852ef4c'
