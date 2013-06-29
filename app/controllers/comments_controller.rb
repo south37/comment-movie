44a71844109725e731d59a34e51875eb65a5ed24
Nao Minami <minami@minami-no-MacBook-Air.local>
2013-06-30 00:55:11 +0900

    redirect with params{url: }

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,7 +44,9 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to movies_url }
+        format.html { redirect_to controller: 'movies',
+          action: 'index',
+          params: {url: session[:movie_url]} }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
