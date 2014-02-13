d3dd9802e82775223eb301d38274e8555b28180d
Nao Minami <gurahu2htsubasa@gmail.com>
2014-02-14 01:31:02 +0900

    include gonを消去。comment formをリファクタ

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,18 +5,17 @@ class MoviesController < ApplicationController
     @movie    = Movie.find_or_create_by_url params[:url]
     @comments = @movie.comments
 
-    @comment    = Comment.new
-    @next_movie = Movie.new
+    @comment  = Comment.new
   end
 
   # POST /movies
   # POST /movies.json
   def create
-    url    = params[:movie][:url].split('=').last
-    @movie = Movie.new(url: url)
+    url   = params[:movie][:url].split('=').last
+    movie = Movie.find_or_create_by_url url
 
-    if @movie.save
-      redirect_to controller: 'movies', action: 'index', params: {url: @movie.url}
+    if movie.save
+      redirect_to controller: 'movies', action: 'index', params: {url: movie.url}
     else
       redirect_to :root, flash: {class: 'alert alert-danger'}
     end
