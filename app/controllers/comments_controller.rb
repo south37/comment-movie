c66908e4b64f179967640b4619a2c08891529bb2
Nao Minami <gurahu2htsubasa@gmail.com>
2014-02-13 22:56:38 +0900

    不要なcommens_controllerのindexとdestroyを消去

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,4 @@
 class CommentsController < ApplicationController
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @comments }
-    end
-  end
-
   # POST /comments
   # POST /comments.json
   def create
@@ -18,17 +7,5 @@ class CommentsController < ApplicationController
     movie.comments << @comment
 
     @comment.save!
-  end
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to comments_url }
-      format.json { head :no_content }
-    end
   end
 end
